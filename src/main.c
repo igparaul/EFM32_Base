33d0fb036551e1d48f381e1b63a0dfcbc2170673
igparaul <igparaul@gmail.com>
2023-05-05 16:58:04 +0200

    Intereconn tasks, lectura del sensor correcta

--- src/main.c
+++ src/main.c
@@ -36,13 +36,15 @@
 #define STACK_SIZE_FOR_TASK    (configMINIMAL_STACK_SIZE + 10)
 #define TASK_PRIORITY          (tskIDLE_PRIORITY + 1)
 
+QueueHandle_t CuaLectura;
+QueueHandle_t CuaPrint;
+
 /* Structure with parameters for LedBlink */
 typedef struct {
   /* Delay between blink of led */
   portTickType delay;
   /* Number of led */
   int          ledNo;
-  uint8_t address;
 } TaskParams_t;
 
 typedef struct {
@@ -54,6 +56,22 @@ typedef struct {
   uint8_t address_Z_1;
   uint8_t address_Z_2;
 } Axis;
+
+typedef struct{
+	int16_t X;
+	int16_t Y;
+	int16_t Z;
+} dataConverted;
+
+uint16_t C2 (uint16_t data)
+{
+	if((data & 0x8000) == 0x8000)
+	{
+		data =~ data;
+		data++;
+	}
+	return data;
+}
 
 /***************************************************************************//**
  * @brief Simple task which is blinking led
@@ -72,7 +90,7 @@ static void LedBlink(void *pParameters)
 
 static void testWhoAmI(void *pParameters)
 {
-  TaskParams_t     * pData = (TaskParams_t*) pParameters;
+  Axis     * pData = (Axis*) pParameters;
   BSP_I2C_Init(pData->address);
 
   bool res = I2C_Test();
@@ -80,30 +98,56 @@ static void testWhoAmI(void *pParameters)
 
 }
 
-static void calculateValues(void *pParameters)
+static void calculateMovement()
+{
+
+}
+
+static void displayMov(void *pParameters)
+{
+	Axis     * pData = (Axis*) pParameters;
+	dataConverted data;
+	for (;;)
+	{
+		xQueueReceive(CuaLectura, &data, 0);
+		printf("Valor X: %d\n", data.X);
+	    printf("Valor Y: %d\n", data.Y);
+	    printf("Valor Z: %d\n\n", data.Z);
+	}
+}
+
+static void ReadValues(void *pParameters)
 {
   Axis     * pData = (Axis*) pParameters;
   BSP_I2C_Init(pData->address);
-  uint16_t data[6];
-  uint16_t dataConvert[3];
+  uint8_t data[6];
+  dataConverted dataConv;
+  int16_t dataConvert[3];
+
+  //I2C_WriteRegister(0x1E, 0x38);
+  //I2C_WriteRegister(0x1F, 0x38);
+  //I2C_WriteRegister(0x24, 0x01);
+
+  I2C_WriteRegister(0x20, 0x20);
 
   for(;;)
   {
-	  I2C_ReadRegister(pData->address_X_1, &data[0]);
-	  I2C_ReadRegister(pData->address_X_2, &data[1]);
-      I2C_ReadRegister(pData->address_Y_1, &data[2]);
-	  I2C_ReadRegister(pData->address_Y_2, &data[3]);
-      I2C_ReadRegister(pData->address_Z_1, &data[4]);
-	  I2C_ReadRegister(pData->address_Z_2, &data[5]);
+	  I2C_ReadRegister(pData->address_X_2, &data[0]);
+	  I2C_ReadRegister(pData->address_X_1, &data[1]);
+      I2C_ReadRegister(pData->address_Y_2, &data[2]);
+	  I2C_ReadRegister(pData->address_Y_1, &data[3]);
+      I2C_ReadRegister(pData->address_Z_2, &data[4]);
+	  I2C_ReadRegister(pData->address_Z_1, &data[5]);
 
-      for (int i = 0; i < 3; i++)
-      {
-            dataConvert[i] = (data[i*2] << 16) + data[i*2+1];
-      }
+	  dataConv.X = (data[0] << 8) + data[1];
+	  //dataConv.X = C2(dataConv.X);
+	  dataConv.Y = (data[2] << 8) + data[3];
+	  //dataConv.Y = C2(dataConv.Y);
+	  dataConv.Z = (data[4] << 8) + data[5];
+	  //dataConv.Z = C2(dataConv.Z);
+
+      xQueueSend(CuaLectura, &dataConv, 0);
   }
-
-  printf("Valor X1: %d\n",(int)res);
-
 }
 
 /***************************************************************************//**
@@ -129,11 +173,14 @@ int main(void)
   SLEEP_SleepBlockBegin((SLEEP_EnergyMode_t)(configSLEEP_MODE + 1));
 #endif
 
+  CuaLectura =  xQueueCreate( 10, sizeof(dataConverted));
+
   /* Parameters value for taks*/
-  static TaskParams_t parametersToTask1 = { pdMS_TO_TICKS(1000), 0, NULL };
-  static TaskParams_t parametersToTask2 = { pdMS_TO_TICKS(500), 1, NULL };
-  static TaskParams_t parametersToTask3 = { NULL, NULL, 0xD6 };
+  static TaskParams_t parametersToTask1 = { pdMS_TO_TICKS(1000), 0 };
+  static TaskParams_t parametersToTask2 = { pdMS_TO_TICKS(500), 1 };
+  static Axis parametersToTask3 = {0xD6, NULL, NULL, NULL, NULL, NULL, NULL };
   static Axis parametersToValues = {0xD6, 0x28, 0x29, 0x2A, 0x2B, 0x2C, 0x2D};
+  static Axis parametersToDisp = {NULL, NULL, NULL, NULL, NULL, NULL, NULL};
 
   /*Create two task for blinking leds*/
   xTaskCreate(LedBlink, (const char *) "LedBlink1", STACK_SIZE_FOR_TASK, &parametersToTask1, TASK_PRIORITY, NULL);
@@ -142,8 +189,11 @@ int main(void)
   // Test WhoAmI
   xTaskCreate(testWhoAmI, (const char *) "testWhoAmI", STACK_SIZE_FOR_TASK, &parametersToTask3, TASK_PRIORITY, NULL);
 
-  // Calcular valors X Y Z
-  xTaskCreate(calculateValues, (const char *) "calculateValues", STACK_SIZE_FOR_TASK, &parametersToValues, TASK_PRIORITY, NULL);
+  // Llegir valors X Y Z
+  xTaskCreate(ReadValues, (const char *) "calculateValues", STACK_SIZE_FOR_TASK, &parametersToValues, TASK_PRIORITY, NULL);
+
+  //
+  xTaskCreate(displayMov, (const char *) "displayMov", STACK_SIZE_FOR_TASK, &parametersToDisp, TASK_PRIORITY, NULL);
 
   /*Start FreeRTOS Scheduler*/
   vTaskStartScheduler();
