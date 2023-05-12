7af69d5a01896538c1a9cd8e30b08bc286c8c76a
igparaul <igparaul@gmail.com>
2023-05-12 16:44:57 +0200

    Versió Entrega Necessària (V.1)

--- src/main.c
+++ src/main.c
@@ -30,22 +30,17 @@
 #include "bsp_trace.h"
 
 #include "sleep.h"
+#include <math.h>
 
 #include "../inc/i2c.h"
 
 #define STACK_SIZE_FOR_TASK    (configMINIMAL_STACK_SIZE + 10)
 #define TASK_PRIORITY          (tskIDLE_PRIORITY + 1)
 
-QueueHandle_t CuaLectura;
-QueueHandle_t CuaPrint;
-
-/* Structure with parameters for LedBlink */
-typedef struct {
-  /* Delay between blink of led */
-  portTickType delay;
-  /* Number of led */
-  int          ledNo;
-} TaskParams_t;
+QueueHandle_t CuaLecturaGraus;
+QueueHandle_t CuaLecturaGs;
+QueueHandle_t CuaPrintGraus;
+QueueHandle_t CuaPrintGs;
 
 typedef struct {
   uint8_t address;
@@ -63,6 +58,23 @@ typedef struct{
 	int16_t Z;
 } dataConverted;
 
+typedef struct{
+	char X_sign;
+	int16_t X_ent;
+	int16_t X_dec;
+	char Y_sign;
+	int16_t Y_ent;
+	int16_t Y_dec;
+	char Z_sign;
+	int16_t Z_ent;
+	int16_t Z_dec;
+} dataInG;
+
+typedef struct{
+	float degX;
+	float degY;
+} dataGraus;
+
 uint16_t C2 (uint16_t data)
 {
 	if((data & 0x8000) == 0x8000)
@@ -77,16 +89,6 @@ uint16_t C2 (uint16_t data)
  * @brief Simple task which is blinking led
  * @param *pParameters pointer to parameters passed to the function
  ******************************************************************************/
-static void LedBlink(void *pParameters)
-{
-  TaskParams_t     * pData = (TaskParams_t*) pParameters;
-  const portTickType delay = pData->delay;
-
-  for (;; ) {
-    BSP_LedToggle(pData->ledNo);
-    vTaskDelay(delay);
-  }
-}
 
 static void testWhoAmI(void *pParameters)
 {
@@ -94,35 +96,95 @@ static void testWhoAmI(void *pParameters)
   BSP_I2C_Init(pData->address);
 
   bool res = I2C_Test();
-  printf("Valor correcte (0/1): %d\n",(int)res);
+  printf("Valor correcte ( No(0)/ Si(1)): %d\n",(int)res);
 
 }
 
-static void calculateMovement()
+static void calculateG()
 {
-
-}
-
-static void displayMov(void *pParameters)
-{
-	Axis     * pData = (Axis*) pParameters;
 	dataConverted data;
+	dataInG dataG;
 	for (;;)
 	{
-		xQueueReceive(CuaLectura, &data, 0);
-		printf("Valor X: %d\n", data.X);
-	    printf("Valor Y: %d\n", data.Y);
-	    printf("Valor Z: %d\n\n", data.Z);
+		xQueueReceive(CuaLecturaGs, &data, 0);
+		dataG.X_ent = data.X * 2 / 32767;
+		dataG.X_dec = (data.X * 2000 / 32767) - (dataG.X_ent * 1000);
+		dataG.Y_ent = data.Y * 2 / 32767;
+		dataG.Y_dec = (data.Y * 2000 / 32767) - (dataG.Y_ent * 1000);
+		dataG.Z_ent = data.Z * 2 / 32767;
+		dataG.Z_dec = (data.Z * 2000 / 32767) - (dataG.Z_ent * 1000);
+
+		if (dataG.X_dec < 0)
+		{
+			dataG.X_dec = -dataG.X_dec;
+			dataG.X_ent = -dataG.X_ent;
+			dataG.X_sign = '-';
+		}
+		else
+			dataG.X_sign = '+';
+
+		if (dataG.Y_dec < 0)
+		{
+			dataG.Y_dec = -dataG.Y_dec;
+			dataG.Y_ent = -dataG.Y_ent;
+			dataG.Y_sign = '-';
+		}
+		else
+			dataG.Y_sign = '+';
+
+		if (dataG.Z_dec < 0)
+		{
+			dataG.Z_dec = -dataG.Z_dec;
+			dataG.Z_ent = -dataG.Z_ent;
+			dataG.Z_sign = '-';
+		}
+		else
+			dataG.Z_sign = '+';
+
+		xQueueSend(CuaPrintGs, &dataG, 0);
 	}
 }
 
-static void ReadValues(void *pParameters)
+static void calculateIncl()
+{
+	dataConverted data;
+	dataGraus dataGraus;
+	float xf, yf, zf;
+	for (;;)
+	{
+		xQueueReceive(CuaLecturaGraus, &data, 0);
+		xf = (float)data.X * 2 / 32767;
+		yf = (float)data.Y * 2 / 32767;
+		zf = (float)data.Z * 2 / 32767;
+
+		dataGraus.degX = atanf(xf/zf)*180/3.14159265359;
+		dataGraus.degY = atanf(yf/zf)*180/3.14159265359;
+		xQueueSend(CuaPrintGraus, &dataGraus, 0);
+	}
+}
+
+static void displayMov()
+{
+	dataGraus data;
+	dataInG dataG;
+	for (;;)
+	{
+		xQueueReceive(CuaPrintGraus, &data, 0);
+		xQueueReceive(CuaPrintGs, &dataG, 0);
+		printf("Inclinacio X: %f graus\n", data.degX);
+		printf("Inclinacio Y: %f graus\n\n", data.degY);
+		printf("Valor X: %c%d.%d G\n", dataG.X_sign, dataG.X_ent, dataG.X_dec);
+	    printf("Valor Y: %c%d.%d G\n", dataG.Y_sign, dataG.Y_ent, dataG.Y_dec);
+	    printf("Valor Z: %c%d.%d G\n\n", dataG.Z_sign, dataG.Z_ent, dataG.Z_dec);
+	}
+}
+
+static void readValues(void *pParameters)
 {
   Axis     * pData = (Axis*) pParameters;
   BSP_I2C_Init(pData->address);
   uint8_t data[6];
   dataConverted dataConv;
-  int16_t dataConvert[3];
 
   //I2C_WriteRegister(0x1E, 0x38);
   //I2C_WriteRegister(0x1F, 0x38);
@@ -140,13 +202,11 @@ static void ReadValues(void *pParameters)
 	  I2C_ReadRegister(pData->address_Z_1, &data[5]);
 
 	  dataConv.X = (data[0] << 8) + data[1];
-	  //dataConv.X = C2(dataConv.X);
 	  dataConv.Y = (data[2] << 8) + data[3];
-	  //dataConv.Y = C2(dataConv.Y);
 	  dataConv.Z = (data[4] << 8) + data[5];
-	  //dataConv.Z = C2(dataConv.Z);
 
-      xQueueSend(CuaLectura, &dataConv, 0);
+      xQueueSend(CuaLecturaGs, &dataConv, 0);
+      xQueueSend(CuaLecturaGraus, &dataConv, 0);
   }
 }
 
@@ -173,27 +233,29 @@ int main(void)
   SLEEP_SleepBlockBegin((SLEEP_EnergyMode_t)(configSLEEP_MODE + 1));
 #endif
 
-  CuaLectura =  xQueueCreate( 10, sizeof(dataConverted));
+  CuaLecturaGs =  xQueueCreate( 10, sizeof(dataConverted));
+  CuaLecturaGraus =  xQueueCreate( 10, sizeof(dataConverted));
+  CuaPrintGs = xQueueCreate( 10, sizeof(dataInG));
+  CuaPrintGraus = xQueueCreate( 10, sizeof(dataGraus));
 
-  /* Parameters value for taks*/
-  static TaskParams_t parametersToTask1 = { pdMS_TO_TICKS(1000), 0 };
-  static TaskParams_t parametersToTask2 = { pdMS_TO_TICKS(500), 1 };
-  static Axis parametersToTask3 = {0xD6, NULL, NULL, NULL, NULL, NULL, NULL };
+  /* Parameters value for tasks*/
+  static Axis parametersWhoAmI = {0xD6, NULL, NULL, NULL, NULL, NULL, NULL };
   static Axis parametersToValues = {0xD6, 0x28, 0x29, 0x2A, 0x2B, 0x2C, 0x2D};
-  static Axis parametersToDisp = {NULL, NULL, NULL, NULL, NULL, NULL, NULL};
-
-  /*Create two task for blinking leds*/
-  xTaskCreate(LedBlink, (const char *) "LedBlink1", STACK_SIZE_FOR_TASK, &parametersToTask1, TASK_PRIORITY, NULL);
-  xTaskCreate(LedBlink, (const char *) "LedBlink2", STACK_SIZE_FOR_TASK, &parametersToTask2, TASK_PRIORITY, NULL);
 
   // Test WhoAmI
-  xTaskCreate(testWhoAmI, (const char *) "testWhoAmI", STACK_SIZE_FOR_TASK, &parametersToTask3, TASK_PRIORITY, NULL);
+  xTaskCreate(testWhoAmI, (const char *) "testWhoAmI", STACK_SIZE_FOR_TASK, &parametersWhoAmI, TASK_PRIORITY, NULL);
 
   // Llegir valors X Y Z
-  xTaskCreate(ReadValues, (const char *) "calculateValues", STACK_SIZE_FOR_TASK, &parametersToValues, TASK_PRIORITY, NULL);
+  xTaskCreate(readValues, (const char *) "readValues", STACK_SIZE_FOR_TASK, &parametersToValues, TASK_PRIORITY, NULL);
 
-  //
-  xTaskCreate(displayMov, (const char *) "displayMov", STACK_SIZE_FOR_TASK, &parametersToDisp, TASK_PRIORITY, NULL);
+  // Calcular valors en G
+  xTaskCreate(calculateG, (const char *) "calculateG", STACK_SIZE_FOR_TASK, NULL, TASK_PRIORITY, NULL);
+
+  // Calcular inclinació eix X i Y
+  xTaskCreate(calculateIncl, (const char *) "calculateIncl", STACK_SIZE_FOR_TASK, NULL, TASK_PRIORITY, NULL);
+
+  // Imprimir valors per pantalla
+  xTaskCreate(displayMov, (const char *) "displayMov", STACK_SIZE_FOR_TASK, NULL, TASK_PRIORITY, NULL);
 
   /*Start FreeRTOS Scheduler*/
   vTaskStartScheduler();
